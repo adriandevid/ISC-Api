cdff1341ea1848017ca8a0f8d3ad462fa07859f3
adrian devid <adriandevid36@gmail.com>
2021-03-26 16:05:06 -0300

    Teste unitario de rota de login

---
+++ tests/ISC.Api.Test/Test/ApiTest.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Hosting;
+using System.Net.Http;
+using ISC.Api.Web;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ISC.Api.Test.Test
+{
+    public class ApiTest
+    {
+        private readonly HttpClient _client;
+
+        public ApiTest()
+        {
+            var server = new TestServer(new WebHostBuilder().UseEnvironment("Development").UseStartup<Startup>());
+            _client = server.CreateClient();
+        }
+
+        [Theory]
+        [InlineData("GET", "", "")]
+        public async Task UsuarioTestAsync(string Method, string login, string senha) {
+            var request = new HttpRequestMessage(new HttpMethod(Method), "Api/Usuario/Login?login="+ login +"&&senha="+ senha +"");
+            var response = await _client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
